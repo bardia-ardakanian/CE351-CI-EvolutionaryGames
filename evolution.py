97803665e8cfe0c4581b996e8c6179b8b8feba14
Bardia Ardakanian <58801017+bardia-ardakanian@users.noreply.github.com>
2023-01-21 02:48:16 +0330

    [update] mutate and crossover
    Co-authored-by: Keivan Ipchi Hagh <keivanipchihagh@gmail.com>

--- evolution.py
+++ evolution.py
@@ -1,4 +1,6 @@
 from player import Player
+import pandas as pd
+from matplotlib import pyplot as plt
 import numpy as np
 import copy
 import random
@@ -9,7 +11,12 @@ class Evolution():
 
     def __init__(self, mode):
         self.mode = mode
-        self.records = []
+        self.records = {
+            "generation": [],
+            "max": [],
+            "min": [],
+            "avg": []
+        }
 
 
     # calculate fitness of players
@@ -19,25 +26,28 @@ class Evolution():
 
 
     def gaussian_noise(self, x: np.ndarray, mu = 0.0, std = 0.1):
-        noise = np.random.normal(mu, std, size = x.shape)
+        return x + np.random.normal(mu, std, size = x.shape)
 
-        threshold = 0.3
-        def random_chocie(x):
-            return 0 if x < threshold else x
-
-        x_noisy = x + np.vectorize(random_chocie)(noise)
-        return x_noisy
 
 
     def mutate(self, child):
 
         # TODO
-        # child: an object of class `Player`
-        child.nn.biases[0] = self.gaussian_noise(child.nn.biases[0])
-        child.nn.biases[1] = self.gaussian_noise(child.nn.biases[1])
-        child.nn.weights[0] = self.gaussian_noise(child.nn.weights[0])
-        child.nn.weights[1] = self.gaussian_noise(child.nn.weights[1])
-        
+        # child: an object of class `Player` 
+        wthresh = 0.35
+        bthresh = 0.7
+        mu = 0.0
+        std = 0.3
+          
+        for i in range(0, 2):
+            prob = np.random.uniform(0, 1)
+            if prob <= wthresh:
+                child.nn.weights[i] = self.gaussian_noise(child.nn.weights[i], mu, std)
+                
+            prob = np.random.uniform(0, 1)
+            if prob <= bthresh:
+                child.nn.biases[i] = self.gaussian_noise(child.nn.biases[i], mu, std)
+            
         return child
 
 
@@ -80,21 +90,25 @@ class Evolution():
         # Weights 1 (horizontal split replacement of mother/father)
         w0_split_point = mother.nn.weights[0].shape[0] // 2
         if random.choice([0, 1]) == 0:
-            child.nn.weights[0][:w0_split_point, :] = random.choice([mother, father]).nn.weights[0][:w0_split_point, :]
+            child.nn.weights[0][:w0_split_point, :] = mother.nn.weights[0][:w0_split_point, :]
+            # child.nn.weights[0][:w0_split_point, :] = random.choice([mother, father]).nn.weights[0][:w0_split_point, :]
         else:
-            child.nn.weights[0][w0_split_point:, :] = random.choice([mother, father]).nn.weights[0][w0_split_point:, :]
+            child.nn.weights[0][w0_split_point:, :] = father.nn.weights[0][w0_split_point:, :]
+            # child.nn.weights[0][w0_split_point:, :] = random.choice([mother, father]).nn.weights[0][w0_split_point:, :]
 
         # Weights 2 (vertical split replacement of mother/father)
         w1_split_point = mother.nn.weights[1].shape[0] // 2
         if random.choice([0, 1]) == 0:
-            child.nn.weights[1][:, w1_split_point:] = random.choice([mother, father]).nn.weights[1][:, w1_split_point:]
+            child.nn.weights[1][:, w1_split_point:] = mother.nn.weights[1][:, w1_split_point:]
+            # child.nn.weights[1][:, w1_split_point:] = random.choice([mother, father]).nn.weights[1][:, w1_split_point:]
         else:
-            child.nn.weights[1][:, :w1_split_point] = random.choice([mother, father]).nn.weights[1][:, :w1_split_point]
+            child.nn.weights[1][:, :w1_split_point] = father.nn.weights[1][:, :w1_split_point]
+            # child.nn.weights[1][:, :w1_split_point] = random.choice([mother, father]).nn.weights[1][:, :w1_split_point]
 
         return child
 
 
-    def next_population_selection(self, players, num_players):
+    def next_population_selection(self, players: list, num_players: int):
 
         # TODO Top-k selection
         # players = self.k_top_selection(players, num_players)
@@ -103,15 +117,16 @@ class Evolution():
         players = self.roulette_wheel_selection(players, num_players)
 
         # TODO (additional): plotting
-        self.records.append({
-            'generation': len(self.records),
-            'best_fitness': players[0].fitness,
-            'worst_fitness': players[-1].fitness,
-            'avg_fitness': np.mean([p.fitness for p in players])
-        })
+        # self.records.append({
+        #     'generation': len(self.records),
+        #     'best_fitness': players[0].fitness,
+        #     'worst_fitness': players[-1].fitness,
+        #     'avg_fitness': np.mean([p.fitness for p in players])
+        # })
 
-        print(self.records[-1])
+        # print(self.records[-1])
 
+        self.save_fitness_history(players)  # Save evo
         return players[:num_players]
 
 
@@ -135,3 +150,24 @@ class Evolution():
         players = np.random.choice(players, num_players, p = fitnesses).tolist()
 
         return sorted(players, key = lambda x: x.fitness, reverse = True)
+
+
+    def save_fitness_history(self, players: list) -> None:
+        """
+            Plot the data and save it to a file
+
+            Parameters:
+                players (list): list of players
+        """
+        fitnesses = [player.fitness for player in players]
+        self.records["max"].append(max(fitnesses))
+        self.records["min"].append(min(fitnesses))
+        self.records["avg"].append(sum(fitnesses) / len(players))
+
+
+    def plot_fitness_history(self) -> None:
+        """
+            Plot the data and save it to a file
+        """
+        pd.DataFrame(data = self.records).plot()
+        plt.show()
